d68fcaa9df2d094c62b6e6498b042dea8b160376
zaki bousmaha <zakib9@gmail.com>
2024-10-01 14:35:49 +0100

    fixing https prblm

--- src/store.js
+++ src/store.js
@@ -23,7 +23,7 @@ const state = reactive({
 
 async function search(lo){
     try {
-        const data = await axios.get(`http://api.weatherapi.com/v1/search.json?key=224f96a6b0f74d7e9d6181034243009&q=${lo}`)
+        const data = await axios.get(`https://api.weatherapi.com/v1/search.json?key=224f96a6b0f74d7e9d6181034243009&q=${lo}`)
         state.currentLocation = data
     } catch (error) {
         state.currentLocation = "Failed to fetch movie data."
@@ -34,7 +34,7 @@ async function weather() {
     if (state.finalLocation) {
         
         try {
-            const data = await axios.get(`http://api.weatherapi.com/v1/current.json?key=224f96a6b0f74d7e9d6181034243009&q=${state.finalLocation}&aqi=no`)
+            const data = await axios.get(`https://api.weatherapi.com/v1/current.json?key=224f96a6b0f74d7e9d6181034243009&q=${state.finalLocation}&aqi=no`)
             state.currentWeather = data
             console.log(data.data.current.temp_c)
         } catch (error) {
